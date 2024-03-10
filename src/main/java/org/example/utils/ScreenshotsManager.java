d69beccf429d0386c63179bd3fc6b7e8f59816d0
SmolD <Smolskydima33@gmail.com>
2024-03-11 00:06:05 +0300

    Implemente unique filenames using thread ID at ScreenshotsManager

--- src/main/java/org/example/utils/ScreenshotsManager.java
+++ src/main/java/org/example/utils/ScreenshotsManager.java
@@ -8,51 +8,51 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ScreenshotsManager {
 
     private static final String SCREENSHOTS_DIR = "src/test/java/screenshots";
 
-    public static void takeScreenshot(WebDriver driver) {
+    public synchronized static void takeScreenshot(WebDriver driver) {
+        long threadId = Thread.currentThread().getId();
+        String screenshotName = "screenshot_" + threadId + ".png";
+        saveScreenshot(driver, screenshotName);
+    }
 
-        String screenshotsDirectory = Paths.get(SCREENSHOTS_DIR).toString();
+    private static void saveScreenshot(WebDriver driver, String screenshotName) {
+        synchronized (ScreenshotsManager.class) {
+            String screenshotsDirectory = Paths.get(SCREENSHOTS_DIR).toString();
+            String screenshotPath = Paths.get(screenshotsDirectory, screenshotName).toString();
+            TakesScreenshot screenshot = (TakesScreenshot) driver;
+            File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
 
-        Path screenshotsPath = Paths.get(screenshotsDirectory);
-        if (!Files.exists(screenshotsPath)) {
             try {
-                Files.createDirectories(screenshotsPath);
-                System.out.println("Directory for screenshots created: " + screenshotsPath);
+                FileUtils.copyFile(srcFile, new File(screenshotPath));
+                System.out.println("Screenshot saved: " + screenshotPath);
             } catch (IOException e) {
-                System.out.println("Directory for screenshots wasn't created: " + e.getMessage());
-                return;
+                System.out.println("Screenshot didn't saved: " + e.getMessage());
             }
-        }
-        String screenshotName = "screenshot.png";
-        String screenshotPath = Paths.get(screenshotsDirectory, screenshotName).toString();
-        TakesScreenshot screenshot = (TakesScreenshot) driver;
-        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
-
-        try {
-            FileUtils.copyFile(srcFile, new File(screenshotPath));
-            System.out.println("Screenshot saved: " + screenshotPath);
-        } catch (IOException e) {
-            System.out.println("Screenshot didn't saved: " + e.getMessage());
         }
     }
 
     public static void takeSource(WebDriver driver) {
+        long threadId = Thread.currentThread().getId();
+        String pageSourceName = "page_source_" + threadId + ".html";
+        savePageSource(driver, pageSourceName);
+    }
 
-        String screenshotsDirectory = Paths.get(SCREENSHOTS_DIR).toString();
-        String pageSource = driver.getPageSource();
-        String pageSourceName = "page_source.html";
-        String pageSourcePath = Paths.get(screenshotsDirectory, pageSourceName).toString();
-        try {
-            Files.write(Paths.get(pageSourcePath), pageSource.getBytes());
-            System.out.println("Page source code saved: " + pageSourcePath);
-        } catch (IOException e) {
-            System.out.println("Could not save the page source code: " + e.getMessage());
+    private static void savePageSource(WebDriver driver, String pageSourceName) {
+        synchronized (ScreenshotsManager.class) {
+            String screenshotsDirectory = Paths.get(SCREENSHOTS_DIR).toString();
+            String pageSource = driver.getPageSource();
+            String pageSourcePath = Paths.get(screenshotsDirectory, pageSourceName).toString();
+            try {
+                Files.write(Paths.get(pageSourcePath), pageSource.getBytes());
+                System.out.println("Page source code saved: " + pageSourcePath);
+            } catch (IOException e) {
+                System.out.println("Could not save the page source code: " + e.getMessage());
+            }
         }
     }
 }
