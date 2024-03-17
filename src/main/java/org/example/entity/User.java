58ed70e68c590688951bffaa42554f9a1537622f
SmolD <Smolskydima33@gmail.com>
2024-03-18 02:53:11 +0300

    Refactor User class using Lombok

--- src/main/java/org/example/entity/User.java
+++ src/main/java/org/example/entity/User.java
@@ -1,45 +1,17 @@
 package org.example.entity;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
 public class User {
 
     private String id;
     private String name;
     private String email;
 
-    public User() {
-    }
-
-    public User(String id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getPassword() {
         return System.getenv("PASSWORD");
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
