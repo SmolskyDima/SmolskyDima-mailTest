dca702fa6f76dfcc8dfb6bf31b840134685ca621
SmolD <Smolskydima33@gmail.com>
2024-04-08 16:49:28 +0300

    Update ContextMenu: Changed Access Modifier of deleteElementButton to Public

--- src/main/java/org/example/pages/pagecomponents/ContextMenu.java
+++ src/main/java/org/example/pages/pagecomponents/ContextMenu.java
@@ -2,13 +2,8 @@ package org.example.pages.pagecomponents;
 
 import org.example.elements.Button;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class ContextMenu {
-
-    private static final Button deleteElementButton = new Button(By.xpath("//span[text()='Удалить']"), "Delete element");
-
-    public static WebElement getDeleteElementButton() {
-        return deleteElementButton.getElement();
-    }
+    public static final Button deleteElementButton = new Button(By.
+            xpath("//span[text()='Удалить']"), "Delete element");
 }
