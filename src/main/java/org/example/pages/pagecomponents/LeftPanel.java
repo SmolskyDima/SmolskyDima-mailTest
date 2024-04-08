a264524b6108ec9c102b6ef49ebef09b3baae9e2
SmolD <Smolskydima33@gmail.com>
2024-04-08 16:58:12 +0300

    Update LeftPanel class

--- src/main/java/org/example/pages/pagecomponents/LeftPanel.java
+++ src/main/java/org/example/pages/pagecomponents/LeftPanel.java
@@ -2,13 +2,9 @@ package org.example.pages.pagecomponents;
 
 import org.example.elements.Button;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 public class LeftPanel {
+    public static final Button trashButton = new Button(By.
+            xpath("//div[@id='doc_tree_trash']"), "Trash button");
 
-    private static final Button trashButton = new Button(By.xpath("//div[@id='doc_tree_trash']"), "Trash button");
-
-    public static WebElement getTrashButton() {
-        return trashButton.getElement();
-    }
 }
