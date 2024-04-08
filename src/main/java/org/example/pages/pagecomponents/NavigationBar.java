af39e60a90228476c17b42f493f9a805a62aa874
SmolD <Smolskydima33@gmail.com>
2024-04-08 16:54:46 +0300

    Update NavigationBar: Remove lettersButton, Keep documentsButton

--- src/main/java/org/example/pages/pagecomponents/NavigationBar.java
+++ src/main/java/org/example/pages/pagecomponents/NavigationBar.java
@@ -2,21 +2,7 @@ package org.example.pages.pagecomponents;
 
 import org.example.elements.Button;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 
 public class NavigationBar {
-
-    private static final Button lettersButton = new Button(By.id("nav-mail"), "Letters");
-
-    private static final Button documentsButton = new Button(By.id("nav-docs"), "Documents");
-
-    public static WebElement getLettersButton() {
-        return lettersButton.getElement();
-    }
-
-    public static WebElement getDocumentsButton() {
-        return documentsButton.getElement();
-    }
-
+    public static final Button documentsButton = new Button(By.id("nav-docs"), "Documents");
 }
