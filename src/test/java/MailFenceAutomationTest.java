888b310caa06f3dd518f607aeb18de4d8cfd145a
SmolD <Smolskydima33@gmail.com>
2024-04-08 08:51:50 +0300

    Update setUp and closeWebDriver methods at MailFenceAutomationTest class

--- src/test/java/MailFenceAutomationTest.java
+++ src/test/java/MailFenceAutomationTest.java
@@ -1,18 +1,18 @@
 import org.example.entity.User;
 import org.example.listener.TestListener;
+import org.example.pages.DocumentsPage;
+import org.example.pages.EmailPage;
+import org.example.pages.LoginPage;
 import org.example.utils.TemporaryFileCreator;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.nio.file.Path;
-import java.time.Duration;
 
+import static org.example.driver.WebDriverWrapper.getDriver;
+import static org.example.driver.WebDriverWrapper.quitDriver;
 import static org.example.utils.UserManager.getUserById;
 
 @Listeners(TestListener.class)
@@ -22,12 +22,7 @@ public class MailFenceAutomationTest {
 
     @BeforeMethod
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-blink-features=AutomationControlled");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get(MAIL_URL);
+        getDriver().get(MAIL_URL);
     }
 
     @Test
@@ -58,6 +53,11 @@ public class MailFenceAutomationTest {
         User user = getUserById("testUser");
         Path filePath = TemporaryFileCreator.createTempFile();
         String subjectOfEmail = TemporaryFileCreator.extractFileName(filePath);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         LoginPage.loginAsUser(user.getName(), user.getPassword());
         EmailPage.clickNewLetterButton();
         EmailPage.enterRecipientEmail(user.getEmail());
@@ -77,10 +77,7 @@ public class MailFenceAutomationTest {
     }
 
     @AfterMethod
-    public void closeWebDriver(ITestContext context) {
-        WebDriver driver = (WebDriver) context.getAttribute("WebDriver");
-        if (driver != null) {
-            driver.quit();
-        }
+    public void closeWebDriver() {
+        quitDriver();
     }
 }
